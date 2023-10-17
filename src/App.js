e5edb2f618f3f5ea6bc5c0cc3f153f470aee2dcf
radheshdusra <radheshkumar.nath2021@vitstudent.ac.in>
2023-10-17 20:44:41 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,50 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
 
 function App() {
+  const [dob, setDob] = useState('');
+  const [age, setAge] = useState('');
+
+  const calculateAge = () => {
+    const today = new Date();
+    const birthDate = new Date(dob);
+    const ageInMilliseconds = today - birthDate;
+    const ageInYears = Math.floor(ageInMilliseconds / (365.25 * 24 * 60 * 60 * 1000));
+    setAge(ageInYears);
+  };
+
+  const handleInputChange = (e) => {
+    setDob(e.target.value);
+  };
+
+  const myDivStyle = {
+    position: "fixed",
+    top: "50%",
+    left: "50%",
+    transform: "translate(-50%, -50%)",
+    textAlign: "center"
+  };
+
+  const myButtonStyle = {
+    backgroundColor: "#007fff",
+    borderRadius: "10%",
+    color: "white",
+    border: "0px",
+    width: "120px",
+    height: "40px"
+  };
+
+  const myInputStyle = {
+    width: "350px",
+    height: "30px"
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={myDivStyle} className="App">
+      <h1>Age Calculator</h1><br />
+      <h5>Enter your date of birth</h5>
+      <input style={myInputStyle} type="date" id="dob" value={dob} onChange={handleInputChange} /><br /><br />
+      <button style={myButtonStyle} onClick={calculateAge}>Calculate Age</button>
+      {age && <h2>You are {age} years old</h2>}
     </div>
   );
 }
